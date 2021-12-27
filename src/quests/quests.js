4b67c0611047090634d0ce80c5f3941ee4210c26
David Saper <david@totango.com>
2021-12-27 13:28:25 +0200

    first commit

---
+++ src/quests/quests.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Quests = () => {
+	
+	return (
+		<div className="quests-container">
+			<div className="quests-title">The Quests</div>
+		</div>
+	)
+}
---
+++ src/wallet/useWallet.js
@@ -0,0 +1,42 @@
+import {useEffect, useState} from 'react';
+import {checkForKeyOwnership} from "./utils/mint-key";
+
+const useWallet = () => {
+	const [walletAddress, setWalletAddress] = useState('');
+	const [hasKey, setHasKey] = useState(null);
+	
+	useEffect(() => {
+		const checkKey = async () => {
+			if (walletAddress) {
+				const res = await checkForKeyOwnership();
+				setHasKey(res);
+			} else {
+				setHasKey(null);
+			}
+		};
+		checkKey();
+	},  [walletAddress]);
+	
+	useEffect(() => {
+		const wallet = localStorage.getItem('wallet');
+		if (wallet && window.ethereum && window.ethereum.selectedAddress) {
+			setWalletAddress(wallet);
+		}
+		const addWalletListener = () => {
+			if (window.ethereum) {
+				window.ethereum.on("accountsChanged", (accounts) => {
+					if (accounts.length > 0) {
+						setWalletAddress([0]);
+					} else {
+						setWalletAddress("");
+					}
+				});
+			}
+		};
+		addWalletListener();
+	}, []);
+	
+	return {walletAddress, setWalletAddress, hasKey, setHasKey};
+};
+
+export default useWallet;
